552512dec4f7fc1e90c5d51501cb4b28781fea83
23142as <23142as@hvhs.school.nz>
2025-04-30 00:27:30 +0000

    Apr 30, 2025, 12:27 AM

--- t02_variables.js
+++ t02_variables.js
@@ -7,9 +7,17 @@
 
  //Variables 
  var Year = 2025;
- 
+console.log (Year);
+
+var Age = 15;
+console.log (Age);
+
   let pocketMoney =3;
   console.log("pocketMoney")
+
+  
+  console.log("Hi Adarsh");
+  console.log("As of", Year, "You are", Age);
   console.log("You have", pocketMoney,"dollars");
  
  
