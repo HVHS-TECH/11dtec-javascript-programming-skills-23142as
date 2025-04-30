02e8c9f060c169e213a43c4bff077fb5c973f1b2
23142as <23142as@hvhs.school.nz>
2025-04-30 00:15:36 +0000

    Apr 30, 2025, 12:15 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,4 +1,21 @@
+/**************************** 
+ Name of task: Intro to Javascript
+ Person who wrote it: Adarsh 
+ Date written: 4/29/2025
+ Purpose: Introduction to Javascript and how it works
+ ****************************/
+console.log("Yuvi relationship, is a rollercaster emotions, some times in deep love and sometimes in anger and revenge.");
+
+//Variables 
+
+
+/****************************
+ Main code 
+****************************/
 
 
 
-console.log("Running t01_introduction.js");
+
+/****************************
+ Functions
+****************************/
---
+++ t02_variables.js
@@ -0,0 +1,22 @@
+/**************************** 
+ Name of task: Intro to variables
+ Person who wrote it: Adarsh 
+ Date written: 4/30/2025
+ Purpose: Introduction to Javascript and how it works
+ ****************************/
+
+ //Variables 
+  let pocketMoney =3;
+  console.log("pocketMoney")
+ 
+ 
+ /****************************
+  Main code 
+ ****************************/
+ 
+ 
+ 
+ 
+ /****************************
+  Functions
+ ****************************/
