5e183b831a8fe9ec16651616c568376052e7fb77
23142as <23142as@hvhs.school.nz>
2025-05-09 02:51:19 +0000

    May 9, 2025, 2:51 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -38,7 +38,7 @@ var answer;
   answer = pocketMoney/Num2
   console.log("Half of your money is",answer )
  
-  alert("Welcome to my program")
+  alert("Welcome to my program" + username + "")
  var username = prompt("What is your name?", "Yuvi the loner")
  
  
