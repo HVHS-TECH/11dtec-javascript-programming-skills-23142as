0a15d21edd60e8bc01945ca2c76e154d7e7e40a8
23142as <23142as@hvhs.school.nz>
2025-05-09 02:35:21 +0000

    May 9, 2025, 2:35 AM

---
+++ t05_alerts_prompts.js
@@ -0,0 +1,46 @@
+/**************************** 
+ Name of task: Intro to alerts and prompts
+ Person who wrote it: Adarsh 
+ Date written: 5/1/2025
+ Purpose: Introduction of alerts and prompts
+ ****************************/
+
+ //Variables 
+ var Year = 2025;
+console.log (Year);
+
+var Born = 2010;
+
+var Age = 15;
+console.log (Age);
+
+var Num1 = 10;
+
+var Num2 = 2;
+
+var answer;
+
+  let pocketMoney =3;
+  console.log("pocketMoney")
+
+  
+  console.log("Hi Adarsh");
+  console.log("As of", Year, "You are", Age);
+  console.log("You were born in", Born)
+  answer = Age + Num1;
+  console.log("In 10 years you will be", answer, "year old")
+  console.log("You have", pocketMoney,"dollars");
+  answer = pocketMoney/Num2
+  console.log("Half of your money is",answer )
+ 
+ 
+ /****************************
+  Main code 
+ ****************************/
+ 
+ 
+ 
+ 
+ /****************************
+  Functions
+ ****************************/
